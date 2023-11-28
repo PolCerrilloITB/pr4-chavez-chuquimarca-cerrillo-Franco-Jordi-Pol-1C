021d5f9687544e9bf9b7b42361780ce39e7a1949
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-28 12:08:53 +0100

    update

--- e2.py
+++ e2.py
@@ -6,3 +6,13 @@ Descripcion: Programa que mostra un triangle amb nombres a les cantonades.
 Cal demanar quina alçada ha de tenir el triangle.
 Els valors permesos per a l'alçada son entre 2 i 9. (ambdós inclosos)
 """
+altura = int(input("Altura del triangle: "))
+if altura <= 9 and altura >= 2:
+    for n in range(1, altura + 1):
+        print(n, end=" ")
+        for j in range(1, n-1):
+            if n == altura:
+                print(n, end=" ")
+            else:
+                print(" ", end=" ")
+        print(n)
