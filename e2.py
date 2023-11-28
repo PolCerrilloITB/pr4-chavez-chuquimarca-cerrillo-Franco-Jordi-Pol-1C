d543feac7e7d1873c55d8039f9a3836fa1ef7936
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-28 10:09:07 +0100

    update

---
+++ e2.py
@@ -0,0 +1,8 @@
+"""
+Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
+28/11/2023
+ASIXc1C M03 UF1
+Descripcion: Programa que mostra un triangle amb nombres a les cantonades.
+Cal demanar quina alçada ha de tenir el triangle.
+Els valors permesos per a l'alçada son entre 2 i 9. (ambdós inclosos)
+"""
