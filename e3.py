a3e557e004154b4d6c72799c2740cf918c001c79
franco.chavez.7e7 <franco.chavez.7e7@itb.cat>
2023-11-28 12:11:38 +0100

    thtr

--- e3.py
+++ e3.py
@@ -6,3 +6,24 @@ Descripcion: Programa que mostra per pantalla la suma de tots els nombres senars
 de tots els nombres parells inferiors a un número límit, que l’usuari introdueix per teclat.
 ( Ex: si el límit és 31 sumaParells 240 i sumaSenars 225)
 """
+limite = int(input())
+nPares = 0
+nImpares = 0
+l = 1
+sPares = 0
+sImpares = 0
+sP = 1
+sI = 1
+for limite in range(1,limite+1):
+    if l%2 == 0:
+         nPares += 1
+    elif l%2 != 0:
+         nImpares += 1
+    l += 1
+for nPares in range(1, nPares+1, 2):
+    sPares = sPares + sP
+    sP += 2
+for nImpares in range(1, nImpares+1, 2):
+    sImpares = sImpares + sI
+    sI += 2
+print(sImpares)
--- e5.py
+++ e5.py
@@ -2,5 +2,15 @@
 Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
 28/11/2023
 ASIXc1C M03 UF1
-Descripcion: Programa que realitza la multiplicació, de dos nombres sencers,  mitjançant sumes
+Descripcion: Programa que demana a l'usuari la introducció de 10 nombres sencers i ha de mostrar,
+al final i per pantalla, quants són positius, quants negatius i quants zero.
 """
+x = int(input())
+y = int(input())
+sumaT = x
+apoyo = 0
+for e in range(1, y+1):
+    sumaT = sumaT + x * apoyo
+    if apoyo == 0:
+        apoyo += 1
+print(sumaT)
