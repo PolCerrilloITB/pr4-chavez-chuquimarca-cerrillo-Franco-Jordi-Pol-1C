0203dac4c24a8c738af65019ac8d412a423205e5
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-30 09:05:42 +0100

    update

--- e4.py
+++ e4.py
@@ -3,15 +3,14 @@ Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
 28/11/2023
 ASIXc1C M03 UF1
 Descripcion: Programa que imprimeix un tauler d’escacs per pantalla.
-Un taulell d’escacs comença amb la casella  Blanca i és de mida 8x8 sempre ;-)
+Un taulell d’escacs comença amb la casella  Blanca i és de mida 8x8 sempre amb respectius numeros i lletras;-)
 """
-
-for x in range(8):
-   for y in range(8):
-       if (x + y) % 2== 0:
-           print("⬜️", end="")
-       else:
-           print ("⬛️", end="")
-   print ()
-
-   h
+for x in range(8, 0, -1):
+    print(x, end="")
+    for y in range(8):
+        if (x + y) % 2 == 0:
+            print("⬜️", end="")
+        else:
+            print("⬛️", end="")
+    print()
+print(" a", "b", "c", "d", "e", "f", "g", "h")
