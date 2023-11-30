9817845c4dca2b937b66df7ad18c49e57a66e976
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-30 08:29:35 +0100

    update

--- e2.py
+++ e2.py
@@ -9,10 +9,13 @@ Els valors permesos per a l'alçada son entre 2 i 9. (ambdós inclosos)
 altura = int(input("Altura del triangle: "))
 if altura <= 9 and altura >= 2:
     for n in range(1, altura + 1):
-        print(n, end=" ")
-        for j in range(1, n-1):
-            if n == altura:
-                print(n, end=" ")
-            else:
-                print(" ", end=" ")
-        print(n)
+        if 1 == n:
+            print(1)
+        else:
+            print(n, end=" ")
+            for j in range(1, n-1):
+                if n == altura:
+                    print(n, end=" ")
+                else:
+                    print(" ", end=" ")
+            print(n)
