8852398fab4f0832498bd0321127d40a75782a52
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-30 09:16:41 +0100

    update

--- e5.py
+++ e5.py
@@ -4,10 +4,12 @@ Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
 ASIXc1C M03 UF1
 Descripcion: Programa que realitza la multiplicació, de dos nombres sencers,  mitjançant sumes
 """
+#Primero definimos las variables que vamos a "multiplicar", el contador que aumenta y una variable de apoyo.
 x = int(input())
 y = int(input())
 sumaT = x
 apoyo = 0
+#Sumamos a una variable misma el numero de veces que es la otra variable
 for n in range(1, y+1):
     sumaT = sumaT + x * apoyo
     if apoyo == 0:
