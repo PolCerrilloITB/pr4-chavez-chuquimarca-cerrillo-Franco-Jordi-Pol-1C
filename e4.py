cc0d4c995a10925b92b5f9a56f9142cf8c721f42
jordi.chuquimarca.7e7 <jordi.chuquimarca.7e7@itb.cat>
2023-11-30 08:49:39 +0100

    t

--- e4.py
+++ e4.py
@@ -5,3 +5,11 @@ ASIXc1C M03 UF1
 Descripcion: Programa que imprimeix un tauler d’escacs per pantalla.
 Un taulell d’escacs comença amb la casella  Blanca i és de mida 8x8 sempre ;-)
 """
+
+for x in range(8):
+   for y in range(8):
+       if (x + y) % 2== 0:
+           print("⬜️", end="")
+       else:
+           print ("⬛️", end="")
+   print ()
