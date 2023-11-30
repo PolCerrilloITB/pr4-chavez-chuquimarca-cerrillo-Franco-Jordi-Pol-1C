28db11d4eff682db94d76881e770c1c3305d9c02
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-30 09:15:02 +0100

    update

--- e4.py
+++ e4.py
@@ -1,6 +1,6 @@
 """
 Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
-28/11/2023
+30/11/2023
 ASIXc1C M03 UF1
 Descripcion: Programa que imprimeix un tauler d’escacs per pantalla.
 Un taulell d’escacs comença amb la casella  Blanca i és de mida 8x8 sempre amb respectius numeros i lletras;-)
--- e5.py
+++ e5.py
@@ -1,6 +1,6 @@
 """
 Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
-28/11/2023
+30/11/2023
 ASIXc1C M03 UF1
 Descripcion: Programa que realitza la multiplicació, de dos nombres sencers,  mitjançant sumes
 """
