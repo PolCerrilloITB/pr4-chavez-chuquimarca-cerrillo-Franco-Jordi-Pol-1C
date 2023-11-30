e2d8cfdc0ea92b63eeaf5a8afb0762414e1a2dc9
jordi.chuquimarca.7e7 <jordi.chuquimarca.7e7@itb.cat>
2023-11-30 09:13:25 +0100

    q

--- e2.py
+++ e2.py
@@ -6,6 +6,11 @@ Descripcion: Programa que mostra un triangle amb nombres a les cantonades.
 Cal demanar quina alçada ha de tenir el triangle.
 Els valors permesos per a l'alçada son entre 2 i 9. (ambdós inclosos)
 """
+
+#Demanem l'altura del triangle en un interval del 2 al 9.
+#Generarà en el eix de les j el número ordinals (nivells del 1 a x altura)
+#Per cada nivel, n'hi haurà un espaïat
+
 altura = int(input("Altura del triangle: "))
 if altura <= 9 and altura >= 2:
     for n in range(1, altura + 1):
