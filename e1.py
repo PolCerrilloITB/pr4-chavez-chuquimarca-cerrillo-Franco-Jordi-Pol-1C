5a6509f368cf3a1839ecf548e2d4b07fb9a1f2f2
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2023-11-30 09:07:18 +0100

    update

--- e1.py
+++ e1.py
@@ -5,11 +5,14 @@ ASIXc1C M03 UF1
 Descripcion: Programa que demana a l'usuari la introducció de 10 nombres sencers i ha de mostrar,
 al final i per pantalla, quants són positius, quants negatius i quants zero.
 """
+# Contadors de positiu, negatiu, i zeros.
 contPos = 0
 contNeg = 0
 contZer = 0
+# Repetició de 10 cops
 for i in range(10):
  nSencers = int(input("Donam 1 numero sencer: "))
+# Regles de positius negatius i zeros
  if nSencers > 0:
   contPos = contPos + 1
  elif nSencers < 0:
